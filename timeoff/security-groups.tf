9a500957cf1f16eed9ca773a838bfceef3f57f99
tlattice <victor.j.cascante@gmail.com>
2022-02-17 23:28:53 -0600

    Allow ssh from any source.

--- timeoff/security-groups.tf
+++ timeoff/security-groups.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.vpc1.cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
